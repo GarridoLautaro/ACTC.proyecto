fdaede472936e7a0482d2d9a2d2f26c722fd940e
GarridoLautaro <laurang2018@gmail.com>
2025-10-11 19:29:16 -0300

    Final seccion de login

--- Pagina/Pantallas/loginn.php
+++ Pagina/Pantallas/loginn.php
@@ -60,5 +60,29 @@ if (!empty($_GET['ok']) && $_GET['ok'] === 'registered') {
   </section>
 </main>
 
+<footer class="pie">
+    <div class="pie-contenedor">
+        <div class="pie-logo"><img src="./Recursos/marcas/logoactc.png" alt="ACTC"></div>
+        <div class="pie-links">
+            <h3>TC</h3>
+            <ul>
+                <li><a href="./inicio.html">Inicio</a></li>
+                <li><a href="./calendario.html">Calendario</a></li>
+                <li><a href="./campeonato.html">Resultados</a></li>
+                <li><a href="./registro.html">Registro</a></li>
+            </ul>
+        </div>
+        <div class="pie-redes">
+            <h3>Seguinos</h3>
+            <div class="redes-iconos">
+                <a href="https://www.instagram.com/actcargentina"><img src="./Recursos/instagram-svgrepo-com (1).svg" alt="Instagram"></a>
+                <a href="https://www.youtube.com/@actcargentina"><img src="./Recursos/youtube-svgrepo-com.svg" alt="YouTube"></a>
+                <a href="https://actc.org.ar/tc/index.html"><img src="./Recursos/global-svgrepo-com.svg" alt="Web"></a>
+            </div>
+        </div>
+    </div>
+    <div class="pie-bottom">© 2025 ACTC. Todos los derechos reservados.</div>
+</footer>
+
 </body>
 </html>
